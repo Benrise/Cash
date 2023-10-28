075157a0aac78cc76c5c139748ab7984240334fd
Benrise <benrise_x@mail.ru>
2023-10-28 15:46:19 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <TheHeader />
+      <main class="main">
+        <router-view></router-view>
+      </main>
+    <TheFooter />
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer">
+        <div class="footer__container">
+            
+        </div>
+    </footer>
+</template>
